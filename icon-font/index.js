4c4fb8df42381b4462161532943d52e261087141
Wangshiqi <xinke57@sina.com>
2019-07-13 10:05:58 +0800

    1.去掉了fontawesome图标样式中无效的样式引用 2.完全兼容iview图标,现在只有设置path才认为是图片,否则都当做字体图标处理,且要写明引用字体前缀,否则默认为'i-icon'

--- icon-font/index.js
+++ icon-font/index.js
@@ -1,26 +1,34 @@
 Component({
-    externalClasses: ['i-class'],
 
-    properties: {
-        isImg: {
-            type: Boolean,
-            value: false
-        },
-        prefix: {
-            type: String,
-            value: 'i-icon'
-        },
-        path: {
-            type: String,
-            value: ''
-        },
-        color: {
-            type: String,
-            value: 'black'
-        },
-        size: {
-            type: Number,
-            value: 24
-        },
-    }
+  /**
+   * 外部样式
+   */
+  externalClasses: ['i-class', 'class'],
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    prefix: {
+      type: String,
+      value: 'i-icon'
+    },
+    type: {
+      type: String,
+      value: ''
+    },
+    path: {
+      type: String,
+      value: ''
+    },
+    color: {
+      type: String,
+      value: 'white'
+    },
+    size: {
+      type: Number,
+      value: 28
+    },
+  },
+
 });
